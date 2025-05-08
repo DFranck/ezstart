8d967cea1a3c2ab213daff6d7afe799436ed513c
DUFOURNET Franck <franckdufournet@hotmail.fr>
2025-05-08 10:59:09 +0700

    feat(i18n): setup locale folder

--- apps/web/i18n/request.ts
+++ apps/web/i18n/request.ts
@@ -1,4 +1,5 @@
 // apps/web/i18n/request.ts
+import merge from 'deepmerge';
 import { getRequestConfig } from 'next-intl/server';
 import { routing } from './routing';
 
@@ -10,11 +11,16 @@ function isSupportedLocale(
 
 export default getRequestConfig(async ({ requestLocale }) => {
   const resolved = await requestLocale;
-
   const locale = isSupportedLocale(resolved) ? resolved : routing.defaultLocale;
+
+  const [common, header, footer] = await Promise.all([
+    import(`../messages/${locale}/common.json`),
+    import(`../messages/${locale}/header.json`),
+    import(`../messages/${locale}/footer.json`),
+  ]);
 
   return {
     locale,
-    messages: (await import(`../messages/${locale}.json`)).default,
+    messages: merge.all([common.default, header.default, footer.default]),
   };
 });
