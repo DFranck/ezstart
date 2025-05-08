617b73f8997f260d60ce64f997ef80d4eb076a8f
DUFOURNET Franck <franckdufournet@hotmail.fr>
2025-05-08 21:11:41 +0700

    feat: add footer

--- apps/web/components/layout/Footer.tsx
+++ apps/web/components/layout/Footer.tsx
@@ -2,9 +2,11 @@
 
 import { useDevice } from '@/hooks/useDevice';
 import { cn } from '@workspace/ui/lib/utils';
+import Link from 'next/link';
 
 export function Footer() {
   const { isMobile } = useDevice();
+
   return (
     <footer
       className={cn(
@@ -12,7 +14,25 @@ export function Footer() {
         { 'mb-20': isMobile }
       )}
     >
-      © {new Date().getFullYear()} ezStart. All rights reserved.
+      <div className='flex flex-col items-center justify-center gap-2 md:flex-row md:justify-between max-w-5xl mx-auto'>
+        <span>
+          © {new Date().getFullYear()} ezStart. Tous droits réservés.
+        </span>
+
+        <div className='flex gap-4'>
+          <Link
+            href='https://github.com/DFranck/ez-start'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='hover:underline'
+          >
+            GitHub
+          </Link>
+          <Link href='/mentions-legales' className='hover:underline'>
+            Mentions légales
+          </Link>
+        </div>
+      </div>
     </footer>
   );
 }
