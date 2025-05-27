997765d28fee7acff158d2366242dc3c56ecb46d
DUFOURNET Franck <franckdufournet@hotmail.fr>
2025-05-27 10:41:20 +0700

    fix(icon): custom-icon clipping

--- packages/ui/components/icon/src/components/icon.tsx
+++ packages/ui/components/icon/src/components/icon.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import type { LucideProps } from 'lucide-react';
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useMemo } from 'react';
 import type { IconProps } from '../types';
 
 export function Icon({
@@ -18,28 +18,27 @@ export function Icon({
 
   if (!iconName) throw new Error('Icon: icon name is missing');
 
-  const DynamicIcon = lazy<React.ComponentType<LucideProps>>(async () => {
-    switch (prefix) {
-      case 'lucide': {
-        const mod = await import('lucide-react');
-        return { default: mod[iconName as keyof typeof mod] };
+  const DynamicIcon = useMemo(() => {
+    return lazy<React.ComponentType<LucideProps>>(async () => {
+      switch (prefix) {
+        case 'lucide': {
+          const mod = await import('lucide-react');
+          return { default: mod[iconName as keyof typeof mod] };
+        }
+        case 'fa': {
+          const mod = await import('react-icons/fa');
+          return { default: mod[iconName as keyof typeof mod] };
+        }
+        case 'custom': {
+          const mod = await import(`../custom-icons/${iconName.toLowerCase()}`);
+          return { default: mod.default };
+        }
+        default: {
+          throw new Error(`Unknown icon library: ${prefix}`);
+        }
       }
-
-      case 'fa': {
-        const mod = await import('react-icons/fa');
-        return { default: mod[iconName as keyof typeof mod] };
-      }
-
-      case 'custom': {
-        const mod = await import(`../custom-icons/${iconName.toLowerCase()}`);
-        return { default: mod.default };
-      }
-
-      default: {
-        throw new Error(`Unknown icon library: ${prefix}`);
-      }
-    }
-  });
+    });
+  }, [name]);
 
   const fallbackSize = props.size ?? 24;
   const iconSize = props.size ?? fallbackSize;
