98bdcbe5ef1d875f8346dd93d48346704a6f2a14
DUFOURNET Franck <franckdufournet@hotmail.fr>
2025-06-13 17:07:27 +0700

    fix: path

--- apps/ezstart/web/app/[locale]/(views)/(statics)/about/page.tsx
+++ apps/ezstart/web/app/[locale]/(views)/(statics)/about/page.tsx
@@ -10,7 +10,7 @@ import {
   Section,
   UL,
 } from '@ezstart/ui/components';
-import { cn } from '@ezstart/ui/LIb';
+import { cn } from '@ezstart/ui/Lib';
 
 import { useTranslations } from 'next-intl';
 type TimeLIneItem = {
