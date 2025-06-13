f5e8d413851b21bec6e1197797d4a50d31ee7b88
DUFOURNET Franck <franckdufournet@hotmail.fr>
2025-06-13 17:14:19 +0700

    fix: rename UL LI

--- apps/ezstart/web/app/[locale]/(views)/ez-features/[feature]/(billing)/dev/components/log.tsx
+++ apps/ezstart/web/app/[locale]/(views)/ez-features/[feature]/(billing)/dev/components/log.tsx
@@ -1,4 +1,4 @@
-import { LI } from '@ezstart/ui/components';
+import { LI, UL } from '@ezstart/ui/components';
 
 const Log = ({ log }: { log: string[] }) => {
   return (
@@ -6,7 +6,7 @@ const Log = ({ log }: { log: string[] }) => {
       <h4 className='font-bold mb-2'>Logs</h4>
       <UL className='text-xs text-gray-500  overflow-auto bg-gray-900 rounded p-2'>
         {log.map((l, i) => (
-          <LIkey={i} size={'sm'}>
+          <LI key={i} size={'sm'}>
             {l}
           </LI>
         ))}
