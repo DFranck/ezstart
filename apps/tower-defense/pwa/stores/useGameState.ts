14a03810ae69b52d13bc130beab6fc10e5a04a30
DUFOURNET Franck <franckdufournet@hotmail.fr>
2025-08-02 23:09:24 +0700

    feat: useGameState persist

--- apps/tower-defense/pwa/stores/useGameState.ts
+++ apps/tower-defense/pwa/stores/useGameState.ts
@@ -1,6 +1,7 @@
 import { computeCoveredCells } from '@/utils/shapeUtils'
 import { PlacedTower, Position, Tower } from '@tower-defense/types'
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 interface GameState {
   towers: PlacedTower[]
@@ -11,27 +12,44 @@ interface GameState {
   addTower: (tower: PlacedTower) => void
   setPath: (path: Position[]) => void
   placeTowerAt: (x: number, y: number, tower: Tower) => void
+  resetGame: () => void
 }
 
-export const useGameState = create<GameState>(set => ({
-  towers: [],
-  draggedTower: null,
-  path: [],
-
-  setDraggedTower: tower => set({ draggedTower: tower }),
-  addTower: tower => set(s => ({ towers: [...s.towers, tower] })),
-  setPath: path => set({ path }),
-
-  placeTowerAt: (x, y, tower) => {
-    const coveredCells = computeCoveredCells(x, y, tower)
-    const placed: PlacedTower = {
-      ...tower,
-      origin: { x, y },
-      coveredCells,
-    }
-    set(s => ({
-      towers: [...s.towers, placed],
+export const useGameState = create<GameState>()(
+  persist(
+    set => ({
+      towers: [],
       draggedTower: null,
-    }))
-  },
-}))
+      path: [],
+      resetGame: () =>
+        set(() => ({
+          towers: [],
+          path: [],
+          draggedTower: null,
+        })),
+      setDraggedTower: tower => set({ draggedTower: tower }),
+      addTower: tower => set(s => ({ towers: [...s.towers, tower] })),
+      setPath: path => set({ path }),
+
+      placeTowerAt: (x, y, tower) => {
+        const coveredCells = computeCoveredCells(x, y, tower)
+        const placed: PlacedTower = {
+          ...tower,
+          origin: { x, y },
+          coveredCells,
+        }
+        set(s => ({
+          towers: [...s.towers, placed],
+          draggedTower: null,
+        }))
+      },
+    }),
+    {
+      name: 'game-state', // Clé dans localStorage
+      partialize: state => ({
+        towers: state.towers,
+        path: state.path,
+      }),
+    }
+  )
+)
