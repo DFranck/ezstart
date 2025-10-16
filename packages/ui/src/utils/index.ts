85108f5ce4677879ba19be5601e528a03ec674ae
DUFOURNET Franck <franckdufournet@hotmail.fr>
2025-10-17 00:56:45 +0700

    fix(ui): export ApiError and HttpMethod types from utils
    Vercel build was failing because ApiError and HttpMethod types weren't exported
    from @ezstart/ui/utils, but were used in apps/ezbill/web/src/utils/api.ts
    
    Added exports to packages/ui/src/utils/index.ts

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -1,4 +1,4 @@
-export { callApi, type ApiResponse, type CallApiOptions } from './call-api'
+export { callApi, type ApiResponse, type ApiError, type HttpMethod, type CallApiOptions } from './call-api'
 export { capitalize } from './capitalize'
 export { getApiUrl, type ApiUrlConfig } from './get-api-url'
 export { runWithFeedback } from './runWithFeedBack'
