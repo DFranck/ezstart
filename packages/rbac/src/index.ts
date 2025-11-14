f746838870ad347577af56b770a5ecd156b15b4b
DUFOURNET Franck <franckdufournet@hotmail.fr>
2025-11-14 16:22:07 +0700

    fix(rbac): remove components export to prevent React import in APIs
    Remove 'export * from ./components' as components are now in @ezstart/rbac-ui package.

--- packages/rbac/src/index.ts
+++ packages/rbac/src/index.ts
@@ -1,10 +1,10 @@
 /**
  * @ezstart/rbac - Role-Based Access Control
  *
- * Universal exports (client + server + React components)
+ * Universal exports (client + server)
+ * For React components, use @ezstart/rbac-ui
  */
 
 export * from './types'
 export * from './client'
 export * from './helpers'
-export * from './components'
